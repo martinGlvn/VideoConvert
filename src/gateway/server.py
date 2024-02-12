8ecd7dc7233de1ed0f58a69dd5fb582d7350848e
martinGlvn <lkn.galvan.martin@gmail.com>
2024-02-12 20:44:27 -0300

    DownloadFileServiceGateway

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -51,4 +51,26 @@ def upload():
     else:
         return "not authorized", 401
 
+@server.route("/download", methods=["GET"])
+def download():
+    access, err = validate.token(request)
 
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+
+    return "not authorized", 401
