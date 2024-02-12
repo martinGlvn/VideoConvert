3f512edcefec7aa734f3bdee23bced5f5fdab053
martinGlvn <lkn.galvan.martin@gmail.com>
2024-02-12 18:00:21 -0300

    ConfigGatewayService

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -5,3 +5,16 @@ from auth import validate
 from auth_svc import access
 from storage import util
 from bson.objectid import ObjectId
+
+
+server = Flask(__name__)
+
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
